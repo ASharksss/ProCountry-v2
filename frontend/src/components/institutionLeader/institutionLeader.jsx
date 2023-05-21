f38a92397990e627de014e312c9a6b08fc0cb86c
tsharksss <tsharksss@gmail.com>
2023-05-21 18:04:18 +0300

    some shit

--- frontend/src/components/institutionLeader/institutionLeader.jsx
+++ frontend/src/components/institutionLeader/institutionLeader.jsx
@@ -1,17 +1,81 @@
 import React from 'react';
+import './institutionLeader.css'
 import avatar from '../../asserts/icons/leader_institute.png'
+import answer from '../../asserts/icons/answer.svg'
+import {Part} from "../../ui/part/part";
+import {Rating} from "../../ui/rating/rating";
 
 const InstitutionLeader = () => {
   return (
-    <div className='institutionLeader'>
-       <div className="institutionLeader_info">
-         <img src={avatar} alt=""/>
+    <div className='institutionLeader card'>
+       <div className="institutionLeader_info row">
+         <img src={avatar} className='institutionLeader_info_img' alt=""/>
          <div className="institutionLeader_info-top">
-           <h1>Имя Фамилия Отчество</h1>
-
+           <div className="row items-center space-between">
+             <h1 className='solid info_title'>Имя Фамилия Отчество</h1>
+             <Rating size={'min'}/>
+           </div>
+           <h2 className='info_job'>Директор школы</h2>
+           <Part/>
          </div>
-
        </div>
+
+      <div className="institutionLeader_education">
+        <h2 className='institutionLeader_education-title'>Образование</h2>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-item'>Уровень профессионального образования:</p>
+          <p className='institutionLeader_education-info'>высшее</p>
+        </div>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-item'>Обучающая организация:</p>
+          <p className='institutionLeader_education-info'>ГАОУ ДПО “Институт развития образования Республики Татарстан</p>
+        </div>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-item'>Специализация (по диплому):</p>
+          <p className='institutionLeader_education-info'>французский и немецкий языки</p>
+        </div>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-item'>Год завершения обучения:</p>
+          <p className='institutionLeader_education-info'>
+            данные не предоставлены
+            <img className='icon_answer' src={answer} alt=""/>
+          </p>
+        </div>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-item'>Курсы:</p>
+          <p className='institutionLeader_education-info'>
+            данные не предоставлены
+            <img className='icon_answer' src={answer} alt=""/>
+          </p>
+        </div>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-item'>Профессиональная переподготовка по направлению:</p>
+          <p className='institutionLeader_education-info'>менеджмент в образовании</p>
+        </div>
+      </div>
+
+      <div className="institutionLeader_awards">
+        <h2 className='institutionLeader_education-title'>Награды</h2>
+        <div className="institutionLeader_awards-list">
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+          <p className='institutionLeader_award-item'>1996г. - Нагрудный знак "Отличник народного просвещения".</p>
+        </div>
+      </div>
+
+      <div className="institutionLeader_social-media">
+        <h2 className='institutionLeader_education-title'>Социальные сети</h2>
+        <div className="row mb-25px space-between">
+          <p className='institutionLeader_education-info'>
+            данные не предоставлены
+            <img className='icon_answer' src={answer} alt=""/>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/institutionPage/institutionPage.jsx
+++ frontend/src/pages/institutionPage/institutionPage.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import './institutionPage.css'
 import InstitutionLeader from "../../components/institutionLeader/institutionLeader";
 
 const InstitutionPage = () => {
   return (
     <div className='institutionPage'>
       <div className="institutionPage_header">
-        <div className="institutionPage_header-top">
-          <h1>Название учреждения</h1>
+        <div className="row space-between">
+          <h1 className='institutionPage-title'>Название учреждения</h1>
           <button>Редактировать</button>
         </div>
-        <div className="institutionPage_header-bottom">
+        <div className="row space-between">
           <div className="bread_crumb">
             тут хлебные крошки
           </div>
         </div>
       </div>
+     {/* <div className="institutionPage_header">
+        <div className="row space-between">
+          <h1 className='institutionPage-title'>Название учреждения</h1>
+        </div>
+        <div className="row space-between">
+          <button className='noBtn'><p className='solid btn-text'>Изменить наименование</p></button>
+          <button className='noBtn'><p className='solid btn-text'>Изменить директора</p></button>
+        </div>
+      </div> v2*/}
+     {/* <div className="institutionPage_header">
+        <div className="row space-between">
+          <h1 className='institutionPage-title'>Название учреждения</h1>
+          <button>Подписаться</button>
+        </div>
+        <div className="row space-between">
+          <div className="bread_crumb">
+            тут хлебные крошки
+          </div>
+          <button className='noBtn'><p className='solid btn-text'>представитель учреждения?</p></button>
+        </div>
+      </div> v1*/}
       <InstitutionLeader/>
     </div>
   );
